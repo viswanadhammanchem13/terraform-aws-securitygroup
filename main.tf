10df3feb1e0f6c10f1fa4316199dabd16e9931f6
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2026-04-01 19:25:25 +0530

    Terraform Sg

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc-id
 
+##Mandating all outbound traffic from the security group to allow internet access to the instances for downloading packages and updates.
+  egress { 
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = merge (
     var.sg_tags,
     local.common_tags,  
